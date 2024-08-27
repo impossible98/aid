d49e9a648289614fc6183b0668b167f862b026b0
impossible98 <christoleavenow@outlook.com>
2024-08-27 18:24:50 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+PORT: int = 6453
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
